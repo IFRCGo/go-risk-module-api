a823ed431099d4385896d985c01a2922cc2329fe
k9845 <khatririshi2430@gmail.com>
2023-01-30 15:21:56 +0545

    Add storm position geojson

---
+++ imminent/management/commands/update_adam_cyclone.py
@@ -0,0 +1,29 @@
+import urllib3
+import json
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+
+from common.models import Country, HazardType
+from imminent.models import Adam
+
+
+class Command(BaseCommand):
+    help = 'Import ADAM Exposure Data'
+
+    def parse_datetime(self, date):
+        return datetime.strptime(date, '%Y-%m-%dT%HH:MM::SS').strftime('%Y-%m-%d')
+
+    def handle(self, *args, **kwargs):
+        http = urllib3.PoolManager()
+        cyclone_event_id = Adam.objects.filter(
+            hazard_type=HazardType.CYCLONE
+        )
+        for event in cyclone_event_id:
+            cyclone_url = f'https://x8qclqysv7.execute-api.eu-west-1.amazonaws.com/dev/events/cyclones/{event.event_id}'
+            response = http.request('GET', cyclone_url)
+            data = response.data
+            cyclone_data = json.loads(data)
+            event.storm_position_geojson = cyclone_data
+            event.save(update_fields=['storm_position_geojson'])
+
