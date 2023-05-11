75b5895aebb64d95c6b547ed83b6a10a055e39ee
k9845 <khatririshi2430@gmail.com>
2023-05-11 10:42:45 +0545

    Refactor data aggregration

--- imminent/management/commands/meteoswiss_agg.py
+++ imminent/management/commands/meteoswiss_agg.py
@@ -17,60 +17,66 @@ class Command(BaseCommand):
     help = 'Aggregated Meteoswiss Data'
 
     def get_latitude_longitude(self, geojson):
-        if geojson:
-            data = geojson['geojson'][0]['footprint_geojson']['features'][0]['geometry']['coordinates']
-            if len(data) > 0:
-                # polygon_co = [tuple(x) for x in data]
-                new_data = [tuple(x) for x in data[0][0]]
-                polygon = json.dumps(mapping(Polygon(new_data).centroid)['coordinates'])
+        if geojson and len(geojson) > 0:
+            data = geojson['footprint_geojson']['features'][0]['geometry']['coordinates']
+            if len(data) == 1:
+                polygon_co = [tuple(x) for x in data[0]]
+                polygon = json.dumps(mapping(Polygon(polygon_co).centroid)['coordinates'])
                 pol = polygon.replace('[', '').replace(']', '')
                 polygon = pol.split(',')
                 return polygon[1], polygon[0]
+            elif len(data) == 2:
+                polygon_co = [tuple(x) for x in data[0][0]]
+                polygon = json.dumps(mapping(Polygon(polygon_co).centroid)['coordinates'])
+                pol = polygon.replace('[', '').replace(']', '')
+                polygon = pol.split(',')
+                return polygon[1], polygon[0]
+            else:
+                return None, None
+        else:
+            return None, None
 
     def handle(self, *args, **kwargs):
         # get the meteoswiss data create an aggregated view for that
         events = MeteoSwiss.objects.values('hazard_name', 'country__name').distinct().annotate(
             start_date=Min('initialization_date'),
             end_date=Max('event_date'),
-            event_details_dict=models.Value({
-                'impacts': [
-                    {
-                        'id': data.id,
-                        'imapct_type': data.impact_type,
-                        'max': data.event_details.get('max'),
-                        'mean': data.event_details.get('mean'),
-                        'min': data.event_details.get('min'),
-                    } for data in MeteoSwiss.objects.filter(
-                        hazard_name=models.F('hazard_name'),
-                        country=models.F('country')
-                    )
-                ]
-            }, output_field=models.JSONField()),
-            geojson_details_dict=models.Value({
-                'geojson': [
-                    {
-                        'id': data.id,
-                        'impact_type': data.impact_type,
-                        'footprint_geojson': data.footprint_geojson,
-                    } for data in MeteoSwiss.objects.filter(
-                        hazard_name=models.F('hazard_name'),
-                        country=models.F('country'),
-                        impact_type='exposed_population_18mps',
-                        initialization_date=models.F('initialization_date'),
-                    )[:1]
-                ]
-            }, output_field=models.JSONField())
         )
         for event in list(events):
-            lat, lon = self.get_latitude_longitude(event['geojson_details_dict'])
+            new_dict = {}
+            for data in MeteoSwiss.objects.filter(
+                    impact_type='exposed_population_18mps',
+                    footprint_geojson__isnull=False,
+            ).order_by('initialization_date')[:1]:
+                new_dict = {
+                    'id': data.id,
+                    'impact_type': data.impact_type,
+                    'footprint_geojson': data.footprint_geojson,
+                }
+            event_details_dict = [
+                {
+                    'id': data.id,
+                    'impact_type': data.impact_type,
+                    'max': data.event_details.get('max'),
+                    'mean': data.event_details.get('mean'),
+                    'min': data.event_details.get('min'),
+                } for data in MeteoSwiss.objects.filter(
+                    hazard_name=event['hazard_name'],
+                    country__name=event['country__name']
+                ).order_by('initialization_date')
+            ]
+            details = {
+                x['impact_type']: x for x in event_details_dict
+            }.values()
+            lat, lon = self.get_latitude_longitude(new_dict)
             data = {
                 'country': Country.objects.filter(name__icontains=event['country__name']).first(),
                 'hazard_name': event['hazard_name'],
                 'start_date': event['start_date'],
-                'event_details': event['event_details_dict'],
+                'event_details': list(details),
                 'hazard_type': HazardType.CYCLONE,
                 'end_date': event['end_date'],
-                'geojson_details': event['geojson_details_dict'],
+                'geojson_details': new_dict,
                 'latitude': lat,
                 'longitude': lon,
             }
