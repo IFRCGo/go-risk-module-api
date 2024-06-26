aa85ba66750d92fa4a4f6659d0a8e4374ed00546
thenav56 <ayernavin@gmail.com>
2024-06-26 10:40:32 +0545

    Fix DJANGO_DEBUG configuration issue

--- risk_module/settings.py
+++ risk_module/settings.py
@@ -48,9 +48,7 @@ env = environ.Env(
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_SECRET_KEY')
+DEBUG = env('DJANGO_DEBUG')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
