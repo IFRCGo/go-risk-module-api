5ce474134b45640e6e25cf0d76820ae710b28b50
thenav56 <ayernavin@gmail.com>
2024-07-06 10:10:35 +0545

    Fix redis locking bug

--- seasonal/tasks.py
+++ seasonal/tasks.py
@@ -1,3 +1,4 @@
+import logging
 from celery import shared_task
 from django.core.management import call_command
 from sentry_sdk.crons import monitor
@@ -6,10 +7,14 @@ from risk_module.sentry import SentryMonitor
 from risk_module.cache import redis_lock, CacheKey
 
 
+logger = logging.getLogger(__name__)
+
+
 @shared_task
-@redis_lock(CacheKey.get_sm_lock(SentryMonitor.CREATE_HAZARD_INFORMATION))
 @monitor(monitor_slug=SentryMonitor.CREATE_HAZARD_INFORMATION)
 def import_think_hazard_informations():
-    call_command(
-        "create_hazard_information",
-    )
+    with redis_lock(CacheKey.get_sm_lock(SentryMonitor.CREATE_HAZARD_INFORMATION)) as acquired:
+        if not acquired:
+            logger.warning('CREATE_HAZARD_INFORMATION is already running....')
+            return
+        call_command("create_hazard_information")
