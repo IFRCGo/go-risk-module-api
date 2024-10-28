e31e36b109ae6a49f284d08737120887034aa7c8
Navin Ayer <thenav56@users.noreply.github.com>
2024-10-28 21:53:33 +0545

    Add helm chart for running on local cluster (#14)

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput &
 python manage.py migrate --noinput
 
-gunicorn risk_module.wsgi:application --bind 0.0.0.0:9007
+gunicorn risk_module.wsgi:application --bind 0.0.0.0:80
