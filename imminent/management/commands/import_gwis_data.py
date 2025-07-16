4121ae3ade1022ede58594de74838f3265442353
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-07-16 10:28:21 +0545

    Update the API; Pull data from 2003 - 2025

--- imminent/management/commands/import_gwis_data.py
+++ imminent/management/commands/import_gwis_data.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
         # Get all the countries with iso3 codes that are not deprecated and independent
         country_iso3 = list(Country.objects.filter(is_deprecated=False, independent=True).values_list("iso3", flat=True))
 
-        for year in range(2024, 2025):
+        for year in range(2003, 2026):
             for iso3 in country_iso3:
                 if iso3:
                     self.import_monthly_data(iso3, year)
@@ -41,7 +41,7 @@ class Command(BaseCommand):
             self.create_gwis_entry(iso3, month, year, GWIS.DSRTYPE.MONTHLY)
 
     def import_cumulative_data(self, iso3, year):
-        url = f"https://api2.effis.emergency.copernicus.eu/statistics/v2/dsr/cumulative?country={iso3.upper()}&year={year}"
+        url = f"https://api2.effis.emergency.copernicus.eu/statistics/v2/dsr/monthly?country={iso3.upper()}&year={year}"
         response = requests.get(url, verify=False)
 
         if response.status_code != 200:
@@ -55,7 +55,7 @@ class Command(BaseCommand):
         cumulative_response = response_data.get("dsrcumulative", [])
 
         for month in cumulative_response:
-            self.create_gwis_entry(iso3, month, year, GWIS.DSRTYPE.CUMULATIVE)
+            self.create_gwis_entry(iso3, month, year, GWIS.DSRTYPE.CUMMULATIVE)
 
     def create_gwis_entry(self, iso3, data, year, dsr_type):
         country = Country.objects.filter(iso3=iso3).first()
